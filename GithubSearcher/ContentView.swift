150253774908451c59b53fb2cefd424f8eefe778
Graham Hargrove <hargrove.graham@yahoo.com>
2020-02-05 22:04:27 -0500

    Initial Commit

---
+++ GithubSearcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubSearcher
+//
+//  Created by Graham Hargrove on 2/5/20.
+//  Copyright © 2020 Graham Hargrove. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
